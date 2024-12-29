8203ec0aca9725dd2af381cd7b76dfaabb37b928
LatrachDev <m.latrach.youcode@gmail.com>
2024-12-29 14:19:55 +0100

    LM-14 display all books

---
+++ app/controllers/books.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once 'app/models/Book.php';
+    require_once 'app/helpers/Database.php';
+
+    $config = include'app/config/database.php';
+    $db = new Database($config['username'], $config['password'], $config['database']);
+
+    if (!$db->connect()) {
+        abort(500);
+    }
+
+    $book = new Book($db);
+    $allBooks = $book->getAllBooks();
+
+    require_once'app/views/books.php';
+
+
+?>
---
+++ app/views/books.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users - View List of Books</title>
+    <script src="/JavaScript/tailwind.js"></script>
+</head>
+<body class="bg-[url('/images/BookBg.webp')] bg-cover bg-center min-h-screen">
+    <div class="min-h-screen flex flex-col items-center justify-center p-4">
+        <div class="bg-black bg-opacity-10 text-white backdrop-blur-lg shadow-lg rounded-lg p-8 max-w-4xl w-full">
+            <h2 class="text-xl font-semibold text-center mb-6">Available Books</h2>
+
+            <?php if(empty($allBooks)) : ?>
+
+                <div>
+                    <p class="text-center">No Books</p>
+                </div>
+
+            <?php else : ?>
+
+            <table class="w-full text-left border-collapse border border-gray-300">
+                <thead>
+                    <tr class="bg-gray-800 text-white">
+                        <th class="p-4 border border-gray-300">Title</th>
+                        <th class="p-4 border border-gray-300">Author</th>
+                        <th class="p-4 border border-gray-300">Created at</th>
+                        <th class="p-4 border border-gray-300">Availability</th>
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white text-black">
+
+                    <?php foreach($allBooks as $book) : ?>
+                        <tr class="border-b border-gray-300">
+                            <td class="p-4 border border-gray-300"><?= $title ?></td>
+                            <td class="p-4 border border-gray-300"><?= $author ?></td>
+                            <td class="p-4 border border-gray-300">Fiction</td>
+                            <td class="p-4 border border-gray-300"><?= $status ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                        
+                </tbody>
+            </table>
+            <?php endif; ?>
+        </div>
+    </div>
+</body>
+</html>
