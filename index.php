1dda7e33e7330d3164f4faabc5bb74d77d9b59e5
Mohamed Karroumi <mkaroumi123@gmail.com>
2025-01-03 12:01:28 +0100

    clean up

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
 session_start();
 
-// Process available books for reservations
-require_once 'app/helpers/Database.php';
-require_once 'app/models/Book.php';
-
-$config = require 'app/config/database.php';
-$db = new Database($config['username'], $config['password'], $config['database']);
-
-if ($db->connect()) {
-    $book = new Book($db);
-    $book->findById(3);
-    // $book->requestReturn();
-}
-
-
 require 'routes.php';
